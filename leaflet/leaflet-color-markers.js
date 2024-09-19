921a34f362b3b2a100fc0b1fac45bdbfabbc325b
Richard Xu <xxu@huskers.unl.edu>
2024-09-18 22:41:06 -0400

    Copied everything from unl-vrt.org PhP code

---
+++ leaflet/leaflet-color-markers.js
@@ -0,0 +1,27 @@
+var blueIcon = new L.Icon({
+	iconUrl: 'leaflet/images/marker-icon-2x-blue.png',
+	shadowUrl: 'leaflet/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+	iconUrl: 'leaflet/images/marker-icon-2x-red.png',
+	shadowUrl: 'leaflet/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
+var greenIcon = new L.Icon({
+	iconUrl: 'leaflet/images/marker-icon-2x-green.png',
+	shadowUrl: 'leaflet/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
+});
+
